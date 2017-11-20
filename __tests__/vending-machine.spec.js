b8442f4aacd73e06f7b70dce7d68835a32e530bd
Kenneth Chow <kennethchow74@gmail.com>
2017-11-19 22:47:17 -0800

    added vending machine functionality and implemented tests for refill supply/change, dispensing, giving change, and printing inventory.

---
+++ __tests__/vending-machine.spec.js
@@ -0,0 +1,194 @@
+const VendingMachine = require("../src/vending-machine");
+/**
+ * 
+ *  <------------- REFILLING INVENTORY TESTS ------------->
+ * 
+ */
+describe("Refilling the inventory", () => {
+  beforeEach(() => {
+    inventory = {
+      coke: {
+        price: 2.75,
+        stock: 6
+      }
+    };
+    start = new VendingMachine();
+  });
+
+  // NEGATIVE TESTS
+  describe("When no item is passed", () => {
+    it("should throw an error", () => {
+      expect(() => start.refillInventory(undefined, 6, 3)).toThrow();
+    });
+  });
+
+  describe("When no stock is passed", () => {
+    it("should throw an error", () => {
+      expect(() => start.refillInventory("item", 1)).toThrow();
+    });
+  });
+
+  describe("When no inputs are passed", () => {
+    it("should throw an error", () => {
+      expect(() => start.refillInventory()).toThrow();
+    });
+  });
+
+  describe("When a negative stock is passed", () => {
+    it("should throw an error", () => {
+      expect(() => start.refillInventory("item", 1, -1)).toThrow();
+    });
+  });
+
+  describe("When a zero stock is passed", () => {
+    it("should throw an error", () => {
+      expect(() => start.refillInventory("item", 2, 0)).toThrow();
+    });
+  });
+
+  describe("When no price is passed", () => {
+    it("should throw an error", () => {
+      expect(() => start.refillInventory("item", undefined, 1)).toThrow();
+    });
+  });
+
+  describe("When a negative price is passed", () => {
+    it("should throw an error", () => {
+      expect(() => start.refillInventory("item", -1.75, 1)).toThrow();
+    });
+  });
+
+  describe("When a zero price is passed", () => {
+    it("should throw an error", () => {
+      expect(() => start.refillInventory("item", 0, 1)).toThrow();
+    });
+  });
+
+  // POSITIVE TESTS
+  describe("When a valid item, stock, and price is passed", () => {
+    it("should increment the given item in inventory by the stock", () => {
+      start.refillInventory("coke", 2.75, 6);
+      expect(start.printInventory()).toEqual(inventory);
+    });
+  });
+
+  describe("When an existing item restock is passed", () => {
+    it("should add the stock to the given inventory", () => {
+      start.refillInventory("coke", 2.75, 6);
+      start.refillInventory("coke", 2.75, 6);
+      inventory.coke.stock = 12;
+
+      expect(start.printInventory()).toEqual(inventory);
+    });
+  });
+});
+
+/**
+ * 
+ *  <------------- DISPENSING INVENTORY TESTS ------------->
+ * 
+ */
+describe("Dispensing Inventory tests", () => {
+  beforeEach(() => {
+    inventory = {
+      coke: {
+        price: 2.75,
+        stock: 6
+      }
+    };
+
+    this.bank = {
+      toonie: 10,
+      loonie: 25,
+      quarter: 10,
+      dime: 20,
+      nickel: 35,
+      penny: 100
+    };
+    start = new VendingMachine();
+  });
+
+  describe("When an invalid item is chosen", () => {
+    it("Should throw an error", () => {
+      start.refillInventory("coke", 2.75, 1);
+      expect(() => start.dispense("item", 5)).toThrow();
+    });
+  });
+
+  describe("When a negative amount of cash is entered", () => {
+    it("Should throw an error", () => {
+      start.refillInventory("coke", 2.75, 1);
+      expect(() => start.dispense("coke", -5)).toThrow();
+    });
+  });
+
+  describe("When a zero amount of cash is entered", () => {
+    it("Should throw an error", () => {
+      start.refillInventory("coke", 2.75, 1);
+      expect(() => start.dispense("coke", 0)).toThrow();
+    });
+  });
+
+  describe("When there is no stock left for the item", () => {
+    it("Should throw an error", () => {
+      expect(() => start.dispense("coke", 3)).toThrow();
+    });
+  });
+
+  // POSITIVE TESTS
+
+  describe("When all inputs are valid and exact cash is given and there is stock", () => {
+    it("Should return the item requested", () => {
+      start.refillInventory("coke", 2.75, 5);
+      expect(start.dispense("coke", 2.75)).toBe("coke");
+    });
+  });
+});
+
+/**
+ * 
+ *  <------------- DISPENSING INVENTORY TESTS ------------->
+ * 
+ */
+describe("Tests for dispensing change", () => {
+  describe("When cash and price are invalid inputs", () => {
+    it("Should throw error", () => {
+      start.refillInventory("coke", 2.75, 5);
+      expect(() => start.dispense("none", -1)).toThrow();
+    });
+  });
+
+  describe("When cash is negative input", () => {
+    it("Should throw error", () => {
+      start.refillInventory("coke", 2.75, 5);
+      expect(() => start.dispense("coke", -1)).toThrow();
+    });
+  });
+
+  describe("When 0 coin is inserted", () => {
+    it("Should throw error", () => {
+      start.refillInventory("coke", 2.75, 5);
+      expect(() => start.dispense("coke", 0)).toThrow();
+    });
+  });
+
+  describe("When item does not exist", () => {
+    it("Should throw error", () => {
+      expect(() => start.dispense("powerade", 5)).toThrow();
+    });
+  });
+
+  describe("When all inputs are valid and extra cash is given and there is stock", () => {
+    it("Should return the item requested", () => {
+      result = {
+        toonie: 2,
+        quarter: 2,
+        dime: 2,
+        penny: 4
+      };
+      start.refillInventory("coke", 2.75, 5);
+      start.resupplyChange("penny", 5);
+      expect(start.giveChange(2.75, 7.49)).toEqual(result);
+    });
+  });
+});
---
+++ src/vending-machine.js
@@ -0,0 +1,142 @@
+/**
+ *  Vending Machine
+ *  v. 20171911
+ * 
+ *  Kenneth Chow
+ */
+
+class VendingMachine {
+  constructor() {
+    this.power = true;
+    this.inventory = {
+      coke: {
+        price: 2.75,
+        stock: 0
+      }
+    };
+
+    this.bank = {
+      toonie: {
+        value: 2,
+        stock: 10
+      },
+      loonie: {
+        value: 1,
+        stock: 25
+      },
+      quarter: {
+        value: 0.25,
+        stock: 10
+      },
+      dime: {
+        value: 0.1,
+        stock: 20
+      },
+      nickel: {
+        value: 0.05,
+        stock: 35
+      },
+      penny: {
+        value: 0.01,
+        stock: 100
+      }
+    };
+  }
+
+  refillInventory(item, price, stock) {
+    if (
+      item === (undefined || null) ||
+      typeof item !== "string" ||
+      stock === undefined ||
+      typeof stock !== "number" ||
+      stock <= 0 ||
+      price === undefined ||
+      typeof price !== "number" ||
+      price <= 0
+    ) {
+      throw new Error("Please supply valid inputs. ie. apple, 6");
+    }
+
+    this.inventory = {
+      [item]: {
+        price,
+        stock: !this.inventory[item]
+          ? stock
+          : this.inventory[item].stock + stock
+      }
+    };
+
+    // return the item that just got added
+    return this.inventory[item];
+  }
+
+  giveChange(price, cash) {
+    let change = {};
+    let changeNeeded = Math.round((cash - price) * 100) / 100;
+
+    if (price <= 0 || cash < price || cash <= 0) {
+      throw new Error("Invalid inputs. Cash must be greater than the price.");
+    }
+
+    // iterate through the piggy bank
+    for (let coin in this.bank) {
+      if (
+        changeNeeded > 0 &&
+        this.bank[coin].stock > 0 &&
+        this.bank[coin].value <= changeNeeded
+      ) {
+        let calculateCoin = Math.floor(changeNeeded / this.bank[coin].value);
+        change[coin] = calculateCoin;
+        changeNeeded =
+          Math.round(
+            (changeNeeded - this.bank[coin].value * change[coin]) * 100
+          ) / 100;
+      }
+    }
+
+    return change;
+  }
+
+  resupplyChange(coin, stock) {
+    if (!this.bank[coin] && stock <= 0) {
+      throw new Error("Please input valid coin and stock. i.e toonie, 5");
+    }
+    this.bank[coin].stock += stock;
+    return this.bank;
+  }
+
+  dispense(item, cash) {
+    // make sure item exists
+    if (!this.inventory[item]) {
+      throw new Error("Please select a valid item.");
+    } else if (this.inventory[item].stock <= 0) {
+      throw new Error("Sorry, out of stock.");
+    } else if (cash <= 0) {
+      throw new Error("Please insert a valid amount of cash.");
+    }
+
+    const price = this.inventory[item].price;
+
+    // should we dispense change?
+    if (cash === price) {
+      // adjust stock
+      this.inventory[item].stock--;
+      return item;
+    } else if (cash > price) {
+      this.inventory[item].stock--;
+      // dispense change
+      this.giveChange(price, cash);
+      return item;
+    }
+  }
+
+  printInventory() {
+    return this.inventory;
+  }
+
+  turnOff() {
+    this.power = false;
+  }
+}
+
+module.exports = VendingMachine;
